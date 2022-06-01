fcc6ee4a997756abd22a769f2506e6f51908915f
Shubham-ar <shubham.jhamb2000@gmail.com>
2022-06-01 15:15:42 -0400

    .

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,11 +1,19 @@
 #include<iostream>
 using namespace std;
 
-int linearSearch(int arr[],int n, int key){
-    for (int i = 0; i < n;i++){
-        if(arr[i]==key){
-            return i;
-            break;
+int binary_Search(int arr[],int n, int key){
+    int s = 0;
+    int e = n-1;
+    while(s<=e){
+        int mid = (s + e) / 2;
+        if (arr[mid]==key){
+            return mid;
+        }
+        else if(arr[mid]>key){
+            e = mid - 1;
+        }
+        else{
+            s = mid + 1;
         }
     }
         return -1;
@@ -23,7 +31,7 @@ int main(){
         cin >> arr[i];
     }
 
-    cout<<linearSearch(arr, n, key);
+    cout<<binary_Search(arr, n, key);
    
     return 0;
 }
