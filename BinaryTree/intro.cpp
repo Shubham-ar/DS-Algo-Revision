e515aa2639af6785e27625a34bf3b88b106a5bd5
Aastha <39433638+Shubham-ar@users.noreply.github.com>
2022-09-06 16:27:29 -0400

    traversal
    Postorder,inorder,preorder

--- BinaryTree/intro.cpp
+++ BinaryTree/intro.cpp
@@ -2,8 +2,63 @@
 
 #include<iostream>
 using namespace std;
+class node{
+    public:
+        int data;
+        node *left;
+        node *right;
+    node(int d){
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
+};
+node *buildTree()
+{
+    int d;
+    cin >> d;
+    if(d==-1){
+        return;
+    }
+    node *root = new node(d);
+    root->left = buildTree();
+    root->right = buildTree();
+
+    return root;
+}
+
+void inOrderPrint(node* root){
+    if(root->data==-1){
+        return;
+    }
+    inOrderPrint(root->left);
+    cout<<root->data<<" ";
+    inOrderPrint(root->right);
+    return;
+}
+void print(node*root){
+    if(root->data==-1){
+        return;
+    }
+    cout << root->data << " ";
+    print(root->left);
+    print(root->right);
+    return;
+}
+void postorderPrint(node*root){
+    if(root->data==-1){
+        return;
+    }
+    postorderPrint(root->left);
+    postorderPrint(root->right);
+    cout << root->data << "";
+}
 
 int main(){
+
+    node *root = buildTree();
+
+    print(root);
 
     return 0;
 }
