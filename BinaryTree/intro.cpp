bef653f92c5b7190ed5746f5f2c9594bc146e720
Aastha <39433638+Shubham-ar@users.noreply.github.com>
2022-12-25 14:40:24 -0500

    .
    .

--- BinaryTree/intro.cpp
+++ BinaryTree/intro.cpp
@@ -150,6 +150,19 @@ int sum_allnodes(node*root){
     return root->data + ls + rs;
     }
 
+// it is the longest distance between any 2 nodes in the tree
+int diameter(node*root){
+    if(root==NULL){
+        return 0;
+    }
+    int h1 = height(root->left);
+    int h2 = height(root->right);
+    int op1 = h1 + h2;
+    int op2 = diameter(root->left);
+    int op3 = diameter(root->right);
+
+    return max(op1,max( op2, op3));
+}
 int main(){
 
     node *root = buildTree();
